df4dfb191a6c33f8082b447b7d9aa139f020d033
Ahmad Rifa'i <arifai209@gmail.com>
2020-02-26 16:30:05 +0700

    [Add] job applicant test

---
+++ api/tests/job_applicant_test/job_applicant.test.js
@@ -0,0 +1,84 @@
+const httpStatus = require('http-status');
+const { chai, server } = require('../test_config');
+const JobApplicantModel = require('../../model/job_applicant_model');
+
+describe('Job Applicant', () => {
+    // before all clear database
+    before(async () => {
+        await JobApplicantModel.deleteMany({});
+    });
+
+    const userData = {
+        username: 'badcat',
+        password: 'freshfish'
+    };
+
+    const testData = {
+        full_name: 'John Doe',
+        age: 25,
+        email: 'john.doe@test.net',
+        phone_num: '62870889911',
+        address: 'Jl. Raya ringroad selatan',
+        served: 'Office Boy',
+        image: 'https://robohash.org/default_image?set=set4'
+    };
+
+    describe('POST /authorize', () => {
+        it('should user logged for job applicant admin', (done) => {
+            chai.request(server).post('/api/v1/auth/user/authorize')
+                .send(userData)
+                .end((err, res) => {
+                    res.should.have.status(httpStatus.OK);
+                    res.body.should.have.property('message').eql('authorized');
+                    userData.token = res.body.result.token;
+                    done();
+                });
+        });
+    });
+
+    describe('POST /job_applicant', () => {
+        it('should send validation error job applicant', (done) => {
+            chai.request(server).post('/api/v1/job_applicant/create')
+                .send({ full_name: testData.full_name, age: testData.age })
+                .end((err, res) => {
+                    res.should.have.status(httpStatus.UNPROCESSABLE_ENTITY);
+                    done();
+                });
+        });
+
+        it('should do success create new job applicant', (done) => {
+            chai.request(server).post('/api/v1/job_applicant/create')
+                .send(testData).end((err, res) => {
+                    res.should.have.status(httpStatus.CREATED);
+                    done();
+                });
+        });
+
+        it('should send conflict job applicant', (done) => {
+            chai.request(server).post('/api/v1/job_applicant/create')
+                .send(testData).end((err, res) => {
+                    res.should.have.status(httpStatus.CONFLICT);
+                    done();
+                });
+        });
+    });
+
+    describe('GET /job_applicant', () => {
+        it('should send forbidden when user not logged', (done) => {
+            chai.request(server).get('/api/v1/job_applicant')
+                .end((err, res) => {
+                    res.should.have.status(httpStatus.FORBIDDEN);
+                    done();
+                });
+        });
+
+        it('should success get all job applicant', (done) => {
+            chai.request(server).get('/api/v1/job_applicant')
+                .set('x-access-token', userData.token)
+                .end((err, res) => {
+                    res.should.have.status(httpStatus.OK);
+                    done();
+                });
+        });
+    });
+});
