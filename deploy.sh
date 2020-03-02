8270de22f5de60fe8c0b8ce5090eff7940816e4d
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-02 15:53:00 +0700

    Add Travis key

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
+eval "$(ssh-agent -s)"
+chmod 600 .travis/id_rsa
+ssh-add .travis/id_rsa
 
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-git push deploy master
+git remote add deploy ssh://git@$IP$DEPLOY_DIR
+git push deploy
 
-# Skip this command if you don't need to execute any additional commands after deploying.
 ssh deploy@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
-  crystal build --release --no-debug index.cr # Change to whatever commands you need!
+  crystal build --release --no-debug index.cr
 EOF
