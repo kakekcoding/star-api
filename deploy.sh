a30fe3c97f66720ce8e72078a40f0127eb68450d
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-02 17:49:23 +0700

    Enhancement deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 eval "$(ssh-agent -s)"
 
 # testing connection travis-ci on server
-ssh -i travis_rsa deploy@HOST pwd
+ssh -i travis_rsa deploy@94.237.67.83 pwd
 
-ssh deploy@$HOST pwd <<EOF
+ssh -i travis_rsa deploy@94.237.67.83 pwd <<EOF
   cd $DEPLOY_DIR
   npm run
 EOF
