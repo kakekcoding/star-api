4ccd2527aca694406af720be53f2f7a6b4fab791
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-03 20:15:52 +0700

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+rm -rf .git
+rm -rf .gitignore
+
+git config --global user.name "Ahmad Rifa'i"
+git config --global user.email "arifai209@gmail.com"
+
+git init .
+git add .
+git commit -m "Deploying"
+git remote add deploy ssh://ubuntu@$HOST/~/ahmadrifai
+git push --force deploy deploy
+
+ssh ubuntu@HOST "cd ~/ahmadrifai/ && npm install && npm run"
