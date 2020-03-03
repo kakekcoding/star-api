cca8ec5b916e12804d245bba6a1022fa53183503
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-03 22:35:38 +0700

    Configure workflow circle-ci

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ git config --global user.email "arifai209@gmail.com"
 git init .
 git add .
 git commit -m "Deploying"
-git remote add deploy ssh://ubuntu@$HOST/~/star-hrd
+git remote add deploy ssh://ubuntu@$HOST:~/star-hrd.git
 git push -u deploy master
 
-ssh ubuntu@$HOST "cd ~/star-hrd/ && npm install && npm run"
+ssh ubuntu@$HOST "cd ~/star-hrd.git/ && npm install && npm run"
