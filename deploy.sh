406b747c23151fe7579b973535a0fe48d597e2fd
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-04 17:37:15 +0700

    Enhancement deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 
 set -e
 
+ssh ubuntu@$HOST 
 cd ~/ahmadrifai/dev.starhrd.site/
 git remote add deploy ssh://ubuntu@$HOST/~/repo/starhrd.git
 git push -u deploy master
-
-ssh ubuntu@$HOST "cd ~/ahmadrifai/dev.starhrd.site/ && npm ci && npm run"
