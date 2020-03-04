1b1e065651a66fd8296678260fc9708e8304dbed
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-04 20:23:30 +0700

    Enhancement deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+set -e
 
-docker build -t arifai209/star-hrd:1.0.0 .
-docker push arifai209/star-hrd:1.0.0
+rm -rf .git
+
+git config --global user.name "Ahmad Rifa'i"
+git config --global user.email "arifai209@gmail.com"
+
+git init .
+git add .
+git commit -m "Deploying"
+git remote add deploy ssh://ubuntu@$HOST:/home/ubuntu/repo/starhrd.git
+git push -u deploy master
 
 ssh ubuntu@$HOST << EOF
-docker pull arifai209/star-hrd:1.0.0
-docker stop arifai209/star-hrd:1.0.0 || true
-docker rm arifai209/star-hrd:1.0.0 || true
-docker rmi arifai209/star-hrd:1.0.0 || true
-docker tag arifai209/star-hrd:1.0.0 arifai209/star-hrd:current
-docker run -d --name star-hrd -p 8080:8080 arifai209/star-hrd:current
+cd ~/ahmadrifai/dev.starhrd.site
+npm install
+cp .env.example .env
+npm start
 EOF
