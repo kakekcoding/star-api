1ebc550b57507343d5672a2d2d9bff4ea2bde177
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-04 20:49:44 +0700

    Test deploy

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git config --global user.email "arifai209@gmail.com"
 git init .
 git add .
 git commit -m "Deploying"
-git remote add deploy ubuntu@$HOST:/home/ubuntu/repo/starhrd.git
+git remote add deploy ssh://ubuntu@$HOST:/home/ubuntu/repo/starhrd.git
 git pull --allow-unrelated-histories
 git push -u deploy master
 
