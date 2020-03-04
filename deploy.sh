9715339b63f6af14d13320b039a4525ec6aefa65
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-04 17:24:13 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-rm -rf .git
-rm -rf .gitignore
-
-git config --global user.name "Ahmad Rifa'i"
-git config --global user.email "arifai209@gmail.com"
-
-git init .
-git add .
-git commit -m "Deploying"
 git remote add deploy ssh://ubuntu@$HOST/~/repo/starhrd.git
 git push -u deploy master
 
