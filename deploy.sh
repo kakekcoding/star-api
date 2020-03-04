eba013bf3b13ba012d1d7841fe864d48c325397f
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-04 17:20:39 +0700

    Test deploy to server

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ git config --global user.email "arifai209@gmail.com"
 git init .
 git add .
 git commit -m "Deploying"
-git remote add deploy ssh://ubuntu@$HOST/~/star-hrd
+git remote add deploy ssh://ubuntu@$HOST/~/repo/starhrd.git
 git push -u deploy master
 
-ssh ubuntu@$HOST "cd ~/star-hrd/ && npm ci && npm run"
+ssh ubuntu@$HOST "cd ~/ahmadrifai/dev.starhrd.site/ && npm ci && npm run"
