eefcc0ae7b21922ba533018a3d87221318c40026
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-04 20:11:07 +0700

    Fixup deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-docker login -username $DOCKER_USER --password-stdin $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 
 docker build -t arifai209/star-hrd:1.0.0 .
 docker push arifai209/star-hrd:1.0.0
 
 ssh ubuntu@$HOST << EOF
 docker pull arifai209/star-hrd:1.0.0
-docker stop api-boilerplate || true
-docker rm api-boilerplate || true
+docker stop arifai209/star-hrd:1.0.0 || true
+docker rm arifai209/star-hrd:1.0.0 || true
 docker rmi arifai209/star-hrd:1.0.0 || true
 docker tag arifai209/star-hrd:1.0.0 arifai209/star-hrd:current
 docker run -d --name star-hrd -p 8080:8080 arifai209/star-hrd:current
