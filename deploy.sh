3ead7a539b11ac0c14c12209d71454583687eb2f
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-05 17:07:47 +0700

    :fire: Lets deploy to server

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
+set -e
 
 echo =======================
 echo Ready deploy to server
-echo =======================
-echo Setting up....
-
-set -e
+echo -e "=======================\n\n"
 
 rm -rf .git
 
@@ -18,16 +16,10 @@ git commit -m "Lets deploy to server...."
 git remote add deploy ubuntu@$HOST:/home/ubuntu/repo/starhrd.git
 git push --force deploy master
 
-echo Connecting to server....
-
 ssh ubuntu@$HOST << EOF
 cd ~/ahmadrifai/dev.starhrd.site
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 12.16.3
-node -e "console.log('Running Node.js ' + process.version)"
 npm install
 cp .env.example .env
-pm2 start api/server.js
+pm2 restart api/server.js
 exit
 EOF
