c8cac50870ea6e10a59ee1c274faac646cba9ed6
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-06 14:51:17 +0700

    Cleanup code

--- api/controller/employee_controller.js
+++ api/controller/employee_controller.js
@@ -3,60 +3,60 @@ const EmployeeModel = require('../model/employee_model');
 const apiResponse = require('../helper/api_response');
 
 exports.create = async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return apiResponse.unprocessableResponse(res, 'unprocessed', errors.array());
-        }
+	try {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return apiResponse.unprocessableResponse(res, 'unprocessed', errors.array());
+		}
 
-        const emailExist = await EmployeeModel.findOne({ email: req.body.email });
-        if (emailExist) return apiResponse.conflictResponse(res, 'email already exist');
+		const emailExist = await EmployeeModel.findOne({ email: req.body.email });
+		if (emailExist) return apiResponse.conflictResponse(res, 'email already exist');
 
-        const phoneExist = await EmployeeModel.findOne({ phone_num: req.body.phone_num });
-        if (phoneExist) return apiResponse.conflictResponse(res, 'phone_num already exist');
+		const phoneExist = await EmployeeModel.findOne({ phone_num: req.body.phone_num });
+		if (phoneExist) return apiResponse.conflictResponse(res, 'phone_num already exist');
 
-        const employee = new EmployeeModel({
-            full_name: req.body.full_name,
-            email: req.body.email,
-            phone_num: req.body.phone_num,
-            address: req.body.address,
-            position: req.body.position,
-            salary: req.body.salary,
-        });
+		const employee = new EmployeeModel({
+			full_name: req.body.full_name,
+			email: req.body.email,
+			phone_num: req.body.phone_num,
+			address: req.body.address,
+			position: req.body.position,
+			salary: req.body.salary,
+		});
 
-        const savedData = await employee.save();
-        return apiResponse.createdResponse(res, 'employee created', savedData);
-    } catch (error) {
-        return apiResponse.errorResponse(res, 'a problem occurred', error);
-    }
+		const savedData = await employee.save();
+		return apiResponse.createdResponse(res, 'employee created', savedData);
+	} catch (error) {
+		return apiResponse.errorResponse(res, 'a problem occurred', error);
+	}
 };
 
 exports.get = async (req, res) => {
-    try {
-        EmployeeModel.get((err, result) => {
-            if (err) return apiResponse.badRequestResponse(res, 'something wrong', err);
+	try {
+		EmployeeModel.get((err, result) => {
+			if (err) return apiResponse.badRequestResponse(res, 'something wrong', err);
 
-            return apiResponse.successResponse(res, '', result);
-        });
-    } catch (error) {
-        return apiResponse.errorResponse(res, 'a problem occurred', error);
-    }
+			return apiResponse.successResponse(res, '', result);
+		});
+	} catch (error) {
+		return apiResponse.errorResponse(res, 'a problem occurred', error);
+	}
 };
 
 exports.getDetail = async (req, res) => {
-    try {
-        EmployeeModel.findById(req.params.id, (err, result) => {
-            if (err) {
-                return apiResponse.badRequestResponse(res, 'something wrong', err);
-            } else if (!result) {
-                return apiResponse.notFoundResponse(res, 'data not found');
-            } else {
-                return apiResponse.successResponse(res, '', result);
-            }
-        });
-    } catch (error) {
-        return apiResponse.errorResponse(res, 'a problem occurred', error);
-    }
+	try {
+		EmployeeModel.findById(req.params.id, (err, result) => {
+			if (err) {
+				return apiResponse.badRequestResponse(res, 'something wrong', err);
+			} else if (!result) {
+				return apiResponse.notFoundResponse(res, 'data not found');
+			} else {
+				return apiResponse.successResponse(res, '', result);
+			}
+		});
+	} catch (error) {
+		return apiResponse.errorResponse(res, 'a problem occurred', error);
+	}
 };
 
 exports.update = async (req, res) => {
@@ -67,10 +67,10 @@ exports.update = async (req, res) => {
 			email: req.body.email,
 			phone_num: req.body.phone_num,
 			address: req.body.address,
-            position: req.body.position,
-            salary: req.body.salary,
-            image: req.body.image,
-            status: req.body.status,
+			position: req.body.position,
+			salary: req.body.salary,
+			image: req.body.image,
+			status: req.body.status,
 			created_at: req.body.created_at,
 			updated_at: req.body.updated_at
 		});
--- api/model/employee_model.js
+++ api/model/employee_model.js
@@ -3,22 +3,22 @@ const mongoose = require('mongoose');
 const image = 'https://robohash.org/default_image?set=set4';
 
 const employeeSchema = mongoose.Schema({
-    full_name: String,
-    email: { type: String, lowercase: true, unique: true },
-    phone_num: { type: String, unique: true },
-    address: String,
-    position: String,
-    salary: Number,
-    image: { type: String, require: true, default: image },
-    status: { type: String, default: 'Employment contract' }
+	full_name: String,
+	email: { type: String, lowercase: true, unique: true },
+	phone_num: { type: String, unique: true },
+	address: String,
+	position: String,
+	salary: Number,
+	image: { type: String, require: true, default: image },
+	status: { type: String, default: 'Employment contract' }
 }, {
-    timestamps: {
-        createdAt: 'created_at',
-        updatedAt: 'updated_at'
-    }
+	timestamps: {
+		createdAt: 'created_at',
+		updatedAt: 'updated_at'
+	}
 });
 
 const Employee = module.exports = mongoose.model('Employee', employeeSchema);
 module.exports.get = (callback, limit) => {
-    Employee.find(callback).limit(limit);
+	Employee.find(callback).limit(limit);
 };
