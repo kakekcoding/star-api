fd536239cb52787beeab3ca1f9b76642cf947f8a
Ahmad Rifa'i <arifai209@gmail.com>
2020-03-06 13:41:13 +0700

    Penyesuaian konfigurasi server

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-echo =======================
-echo Ready deploy to server
-echo "=======================\n\n"
-
 rm -rf .git
 
 git config --global user.name "Ahmad Rifa'i"
@@ -13,14 +9,11 @@ git config --global user.email "arifai209@gmail.com"
 git init .
 git add .
 git commit -m "Lets deploy to server...."
-git remote add deploy ubuntu@$HOST:/home/ubuntu/repo/starhrd.git
+git remote add deploy $USER@$HOST:/home/$USER/repo/starhrd.git
 git push --force deploy master
 
-ssh ubuntu@$HOST << EOF
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 12.16.3
-cd ~/ahmadrifai/dev.starhrd.site
+ssh $USER@$HOST << EOF
+cd ~/ahmadrifai/starhrd.site
 npm install
-pm2 restart api/server.js && exit
+npm run pm2 && exit
 EOF
